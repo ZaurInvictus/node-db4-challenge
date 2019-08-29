b7ce7d91cc5973093a2dcf4c882934edc5eead6d
Zaur <42130952+ZaurInvictus@users.noreply.github.com>
2019-08-29 11:05:36 -0500

    db scheme created and migrated to the latest

--- data/migrations/20190828122353_recipes.js
+++ data/migrations/20190828122353_recipes.js
@@ -1,8 +1,74 @@
 
 exports.up = function(knex) {
-  
+  return knex.schema
+
+  .createTable('recipes', table => {
+    table.increments()
+
+    table
+    .string('recipe_name', 128)
+    .notNullable()
+    .unique()
+  })
+
+  .createTable('ingredients', table => {
+     table.increments()
+
+     table
+     .string('ingredient_name', 256)
+     .notNullable()
+  })
+
+
+  .createTable('shoppingList', table => {
+     table.increments()
+
+     table
+      .integer('recipe_id')
+      .unsigned()
+      .notNullable()
+      .references()
+      .inTable('recipes')
+      .onDelete('RESTRICT')
+      .onUpdate('CASCADE')
+
+     table
+      .integer('ingredient_id')
+      .unsigned()
+      .notNullable()
+      .references()
+      .inTable('ingredients')
+      .onDelete('RESTRICT')
+      .onUpdate('CASCADE')
+  })
+
+  .createTable('instructions', table => {
+     table.increments()
+
+     table
+      .integer('recipe_id')
+      .unsigned()
+      .notNullable()
+      .references()
+      .inTable('recipes')
+      .onDelete('RESTRICT')
+      .onUpdate('CASCADE')
+
+      table
+      .string('instruction_desc', 512).notNullable()
+  })
 };
 
 exports.down = function(knex) {
-  
+  // reverse order of creation
+  return knex.schema
+    .dropTableIfExists('instructions')
+    .dropTableIfExists('shoppingList')
+    .dropTableIfExists('ingredients')
+    .dropTableIfExists('recipes')
 };
+
+
+
+
+
